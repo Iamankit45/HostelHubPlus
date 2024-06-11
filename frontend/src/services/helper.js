b1e7bbcb4dfad1d96510a6710b748240baada34b
ANKIT <kumarankit0327@gmail.com>
2024-06-11 20:31:55 +0530

    change Base url for deployment

--- frontend/src/services/helper.js
+++ frontend/src/services/helper.js
@@ -1,4 +1,6 @@
-export const BASE_URL = 'http://localhost:4000'
+// export const BASE_URL = 'http://localhost:4000'
+export const BASE_URL = 'https://hostelhubplus.onrender.com'
+
 
 export const isHostelAdmin = (user) => {
     return user && user.role === 'hostel-admin';
