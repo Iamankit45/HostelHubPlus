a91e556945d6195b4cb0ef2b382545677eb8b841
ANKIT <kumarankit0327@gmail.com>
2024-06-11 20:40:48 +0530

    modified allowedOrigin for deployment server

--- backend/utils/allowedOrigins.js
+++ backend/utils/allowedOrigins.js
@@ -3,9 +3,8 @@ const allowedOrigins = [
     'http://127.0.0.1:8000',
     'http://localhost:3500',
     'http://localhost:3000',
-    'http://localhost:4000',
-    'https://taste-on-way.netlify.app',
-    'https://tasteonway.onrender.com'
+    'http://localhost:4000', 
+    'https://hostel-hub-plus.vercel.app'
 ];
 
 module.exports = allowedOrigins;
